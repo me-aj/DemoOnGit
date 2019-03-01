5a301935c70975c6c24bafa6369a72e2b6c61719
AJAY <39895411+me-aj@users.noreply.github.com>
2019-03-01 10:27:02 +0530

    in user class - for city field setter/getter methods added.

--- GitExample/src/com/aj/User.java
+++ GitExample/src/com/aj/User.java
@@ -11,5 +11,13 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
 	
 }
